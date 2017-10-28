44cee84b068bf4898b35c75ef59375cc3a770c04
Winson Chan <m4d.coder@gmail.com>
2017-10-28 01:16:55 +0000

    Refactor mistral runner to support callback
    Refactor mistral runner to support callback from mistral instead of relying on st2resultstracker. This reduces the unnecessary traffic and CPU time by querying the mistral API. Unit and integration tests are refactored accordingly.

--- st2client/st2client/commands/action.py
+++ st2client/st2client/commands/action.py
@@ -1335,9 +1335,11 @@ class ActionExecutionPauseCommand(ActionRunCommandMixin, ActionExecutionReadComm
             resource.get_plural_display_name().lower(),
             *args, **kwargs)
 
-        self.parser.add_argument('id', nargs='+',
-                                 metavar='id',
-                                 help='ID of action execution to pause.')
+        self.parser.add_argument(
+            'id',
+            metavar='id',
+            help='ID of %s to pause.' % resource.get_display_name().lower()
+        )
 
         self._add_common_options()
 
@@ -1369,9 +1371,11 @@ class ActionExecutionResumeCommand(ActionRunCommandMixin, ActionExecutionReadCom
             resource.get_plural_display_name().lower(),
             *args, **kwargs)
 
-        self.parser.add_argument('id', nargs='+',
-                                 metavar='id',
-                                 help='ID of action execution to resume.')
+        self.parser.add_argument(
+            'id',
+            metavar='id',
+            help='ID of %s to resume.' % resource.get_display_name().lower()
+        )
 
         self._add_common_options()
 
